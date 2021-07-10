e899ced30e469efd701ed8dad31860c19f31cca9
adesire-dev <akim.desire.dev@gmail.com>
2021-07-10 14:58:14 -0400

    feat(game): main game loop established

--- src/components/game/GameView.jsx
+++ src/components/game/GameView.jsx
@@ -1,18 +1,77 @@
-import React from 'react';
-import Player from '../../Game/Player/Player';
+import React, { useState, useEffect } from 'react';
 import { calculateRoundWinner } from '../../Game/GameManager';
 
 import PlayerContainer from './PlayerContainer';
 import './GameView.scss';
 
 const GameView = () => {
-    const PlayerOne = new Player({ name: "Marco", health: 102 });
-    const PlayerTwo = new Player({ name: "Akim", health: 100 });
+    const newPlayerOne = { name: "Marco", health: 102, attack: 34 };
+    const newPlayerTwo = { name: "Akim", health: 100, attack: 34 };
+    const [player1Choice, setPlayer1Choice] = useState(null);
+    const [player2Choice, setPlayer2Choice] = useState(null);
+    const [playerOne, updatePlayerOne] = useState(newPlayerOne);
+    const [playerTwo, updatePlayerTwo] = useState(newPlayerTwo);
+
+    useEffect(() => {
+        const handleCombat = (player1Choice, player2Choice) => {
+            const winner = calculateRoundWinner(player1Choice, player2Choice);
+    
+            if (winner === 'player1') {
+                const { health } = playerTwo;
+                const { attack } = playerOne;
+                const updatedPlayerTwo = {
+                    ...playerTwo,
+                    health: health - attack,
+                }
+                updatePlayerTwo(updatedPlayerTwo);
+            } else if (winner === 'player2') {
+                // clean this up into a utility function
+                const { health } = playerOne;
+                const { attack } = playerTwo;
+                const updatedPlayerOne = {
+                    ...playerOne,
+                    health: health - attack,
+                }
+                updatePlayerOne(updatedPlayerOne);
+            }
+    
+            setPlayer1Choice(null);
+            setPlayer2Choice(null);
+            // Reset Timer
+        }
+
+        if (player1Choice && player2Choice) {
+            handleCombat(player1Choice, player2Choice)
+        }
+    }, [
+        player1Choice, player2Choice, playerOne, playerTwo
+    ])
+
+
+    // allow player1 to make selection
+    // allow player2 to make selection
+    // if both players have made a selection, call handleCombat
+    // if a player won, calculateDamage
+    // apply damage to losing player
+    // if Losing player health <= 0, game ends
+
+    // Set timer on handleCombat
+    // ? If player does not choose before timer, they automatically lose
+
+
 
     return (
         <div className='gameView'>
-            <PlayerContainer player={PlayerOne} />
-            <PlayerContainer player={PlayerTwo} />
+            <PlayerContainer
+                player={playerOne}
+                makeChoice={(choice) => setPlayer1Choice(choice)}
+                currentChoice={player1Choice}
+            />
+            <PlayerContainer
+                player={playerTwo}
+                makeChoice={(choice) => setPlayer2Choice(choice)}
+                currentChoice={player2Choice}
+            />
         </div>
     )
 };
--- src/components/game/PlayerChoiceButtons.jsx
+++ src/components/game/PlayerChoiceButtons.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { string, func } from 'prop-types';
 import '../../utils/commonStyles.scss';
 
-const PlayerChoiceButtons = ({ currentSelection, updateCurrentSelection }) => {
-    const getStatusClass = (option) => {
-        return option === currentSelection ? 'active' : '';
+const PlayerChoiceButtons = ({ currentChoice, makeChoice }) => {
+    const getStatusClass = (buttonOption) => {
+        return buttonOption === currentChoice ? 'active' : '';
     } 
 
     return (
         <div className="flex flex-direction-row flex-justify-around">
-            <button name="rock" className={getStatusClass('rock')} onClick={() => updateCurrentSelection('rock')} />
-            <button name="paper" className={getStatusClass('paper')} onClick={() => updateCurrentSelection('paper')} />
-            <button name="scissors" className={getStatusClass('scissors')} onClick={() => updateCurrentSelection('scissors')} />
+            <button name="rock" className={getStatusClass('rock')} onClick={() => makeChoice('rock')}>
+                ROCK
+            </button>
+            <button name="paper" className={getStatusClass('paper')} onClick={() => makeChoice('paper')} >
+                PAPER
+            </button>
+            <button name="scissors" className={getStatusClass('scissors')} onClick={() => makeChoice('scissors')} >
+                SCISSORS
+            </button>
         </div>
     )
 }
@@ -20,3 +26,5 @@ PlayerChoiceButtons.propTypes = {
     currentSelection: string.isRequired,
     updateCurrentSelection: func.isRequired,
 }
+
+export default PlayerChoiceButtons;
